43811e1e52ca4e0fe4b966e90281cf4eab722a1b
Sultaan Shah <seshah@uwaterloo.ca>
2017-02-26 14:17:23 -0500

    added books from bookstore

--- scripts/books.sh
+++ scripts/books.sh
@@ -1,18 +1,186 @@
 #!/bin/bash
-
-curl -H "Content-Type: application/json" -X POST -d '{
-  "title": "How to Express JS",
-  "courses": [
-	"CS446"
-  ],
-  "isbn": "9782123456803"
-}' http://localhost:3000/api/book/create
-
-curl -H "Content-Type: application/json" -X POST -d '{
-  "title": "No fear, Shakespeare",
-  "courses": [
-	"ENGL101",
-	"ESL401"
-  ],
-  "isbn": "9782123456805"
-}' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"LEARNING PROCESSING 2ND ED                        "
+    ,"isbn":"9780123944436"
+    ,"courses":["CS106"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"I-CLICKER + RF RESPONSE REMOTE WITH 5 YEAR REEF AC"
+    ,"isbn":"9781498602846"
+    ,"courses":["CS106", "CS115", "CS116", "CS135", "CS136", "CS251"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"HOW TO DESIGN PROGRAMS                            "
+    ,"isbn":"9780262062183"
+    ,"courses":["CS115", "CS135", "CS136"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"THINK PYTHON : AN INTRODUCTION TO SOFTWARE DESIGN "
+    ,"isbn":"9780999012352"
+    ,"courses":["CS116"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"C PROGRAMMING : A MODERN APPROACH 2ED             "
+    ,"isbn":"9780393979503"
+    ,"courses":["CS136", "CS146"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"ABSOLUTE C++ 6TH ED LOOSELEAF                     "
+    ,"isbn":"9780133970982"
+    ,"courses":["CS138"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"ABSOLUTE C++ 6TH ED PKG W/ MYPROGRAMMINGLAB       "
+    ,"isbn":"9780134225395"
+    ,"courses":["CS138", "CS246"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"ALGORITHMS IN C++, PARTS 1-4 3ED                  "
+    ,"isbn":"9780201350883"
+    ,"courses":["CS240"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"COMPUTER ORGANIZATION & DESIGN 4ED                "
+    ,"isbn":"9780123744937"
+    ,"courses":["CS241"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"COMPUTER ORGANIZATION AND DESIGN 5ED              "
+    ,"isbn":"9780124077263"
+    ,"courses":["CS241", "CS251"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"LOGIC IN COMPUTER SCIENCE 2ED                     "
+    ,"isbn":"9780521543101"
+    ,"courses":["CS245"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"MATHEMATICAL LOGIC FOR COMPUTER SCIENCE 2ED       "
+    ,"isbn":"9789810230913"
+    ,"courses":["CS245"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"FUNDAMENTALS OF DATABASE SYSTEMS 7TH ED           "
+    ,"isbn":"9780133970777"
+    ,"courses":["CS338"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"INTRODUCTION TO ALGORITHMS 3ED                    "
+    ,"isbn":"9780262033848"
+    ,"courses":["CS341"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"DATABASE MANAGEMENT SYSTEMS 3ED                   "
+    ,"isbn":"9780072465631"
+    ,"courses":["CS348"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"BUILDING INTERACTIVE SYSTEMS                      "
+    ,"isbn":"9781423902485"
+    ,"courses":["CS349"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"INTRODUCTION TO THE THEORY OF COMPUTATION 3RD ED  "
+    ,"isbn":"9781133187790"
+    ,"courses":["CS365"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"180-DAY ETEXT FOR CRAFTING A COMPILER             "
+    ,"isbn":"9780133001570"
+    ,"courses":["CS444", "CS644"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"CRAFTING A COMPILER                               "
+    ,"isbn":"9780136067054"
+    ,"courses":["CS444", "CS644"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"E-TEXT FOR CRAFTING A COMPILER                    "
+    ,"isbn":"9780132634540"
+    ,"courses":["CS444", "CS644"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"PARALLEL COMPUTER ORGANIZATION AND DESIGN         "
+    ,"isbn":"9780521886758"
+    ,"courses":["CS450", "CS650"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"180-DAY E-TEXT FOR DISTRIBUTED SYSTEMS 5TH        "
+    ,"isbn":"9780133001372"
+    ,"courses":["CS454"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"DISTRIBUTED SYSTEMS : CONCEPTS & DESIGN 5TH       "
+    ,"isbn":"9780132143011"
+    ,"courses":["CS454"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"DISTRIBUTED SYSTEMS : PRINCIPLES AND PARADIGMS 2ED"
+    ,"isbn":"9780132392273"
+    ,"courses":["CS454"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"E-TEXT FOR DISTRIBUTED SYSTEMS 5TH                "
+    ,"isbn":"9780132773577"
+    ,"courses":["CS454"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"SECOND courses IN FORMAL LANGUAGES & AUTOMATA THEOR"
+    ,"isbn":"9780521865722"
+    ,"courses":["CS462", "CS662"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"INTRODUCTION TO QUANTUM COMPUTING                 "
+    ,"isbn":"9780198570493"
+    ,"courses":["CS467"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"ARTIFICIAL INTELLIGENCE : A MODERN APPROACH 3ED   "
+    ,"isbn":"9780136042594"
+    ,"courses":["CS486","CS686"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"ARTIFICIAL INTELLIGENCE : FOUNDATIONS OF COMPUTATI"
+    ,"isbn":"9780521519007"
+    ,"courses":["CS486", "CS686"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"COMPUTER GRAPHICS 3RD ED                          "
+    ,"isbn":"9780321399526"
+    ,"courses":["CS488"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"COMPUTER GRAPHICS WITH OPEN GL 4ED                "
+    ,"isbn":"9780136053583"
+    ,"courses":["CS488"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"OPENGL PROGRAMMING GUIDE VERSION 4.5 WITH SPIR-V 9"
+    ,"isbn":"9780134495491"
+    ,"courses":["CS488"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"E-TEXT FOR MANAGING AND USING INFORMATION SYSTEMS "
+    ,"isbn":"9781119255215"
+    ,"courses":["CS490"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"MANAGING AND USING INFORMATION SYSTEMS 6TH ED (LOO"
+    ,"isbn":"9781119244288"
+    ,"courses":["CS490"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"MANAGING AND USING INFORMATION SYSTEMS: A STRATEGI"
+    ,"isbn":"9781118281734"
+    ,"courses":["CS490"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"BIOMEDICAL INFORMATICS 4TH ED                     "
+    ,"isbn":"9781447144731"
+    ,"courses":["CS792"]
+ }' http://localhost:3000/api/book/create
+ curl -H "Content-Type: application/json" -X POST -d '{
+     "title":"AUTOMATIC SEQUENCES : THEORY APPLICATIONS         "
+    ,"isbn":"9780521823326"
+    ,"courses":["CS860"]
+ }' http://localhost:3000/api/book/create
